52d6a658e9af31ad1aef034d2bb188bcb622dde5
Sunnatillo <sunnatillo19912006@gmail.com>
2024-06-14 21:17:51 +0500

    initial

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from fastai.vision.all import *
+import pathlib
+import plotly
+import plotly.express as px
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+
+#title
+st.title("Transportni klassifikatsiya qiluvchi model")
+
+
+# rasmni joylash
+file = st.file_uploader("Rasm yuklash", type=['png', 'jpg', 'gif', 'svg'])
+if file:
+    st.image(file)
+    # PIL convert
+    img = PILImage.create(file)
+    
+    # modelni yuklash
+    model = load_learner('transport_model.pkl')
+    
+    # bashorat qilish prediction
+    pred, pred_id, probs = model.predict(img)
+    st.success(f"Bashorat: {pred}")
+    st.info(f"Ehtimollik: {probs[pred_id]*100:.1f}%")
+    
+    # plotting
+    
+    fig = px.bar(x= probs*100, y = model.dls.vocab)
+    st.plotly_chart(fig)
